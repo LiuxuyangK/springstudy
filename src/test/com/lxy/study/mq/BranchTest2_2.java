0dceb19b94a7e3d6317b0c562f8a6118f8e3c894
Harry <xuyang.liu@ele.me>
2018-05-19 12:02:01 +0800

    下一次（第二次）提交，写了main方法

--- src/test/com/lxy/study/mq/BranchTest2_2.java
+++ src/test/com/lxy/study/mq/BranchTest2_2.java
@@ -4,4 +4,12 @@ package com.lxy.study.mq;
  * test2 分支上创建的
  */
 public class BranchTest2_2 {
+
+    /**
+     * 下一次（第二次）提交，写了main方法
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
 }
