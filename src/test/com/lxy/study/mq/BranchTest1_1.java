34f2a78d3e58775126fd70bc26286749c1831e9d
Harry <xuyang.liu@ele.me>
2018-05-19 11:50:36 +0800

    在这个写了一个main方法

--- src/test/com/lxy/study/mq/BranchTest1_1.java
+++ src/test/com/lxy/study/mq/BranchTest1_1.java
@@ -4,4 +4,11 @@ package com.lxy.study.mq;
  * 在test1分支上创建的
  */
 public class BranchTest1_1 {
+    /**
+     * 准备在test1分支上搞点事情
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
 }
