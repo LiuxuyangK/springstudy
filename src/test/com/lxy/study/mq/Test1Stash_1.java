d0d6958e5cfee3d4836b6f62f4b8b930c8a5b7d0
Harry <xuyang.liu@ele.me>
2018-05-19 12:12:55 +0800

    提交一下？

--- src/test/com/lxy/study/mq/Test1Stash_1.java
+++ src/test/com/lxy/study/mq/Test1Stash_1.java
@@ -4,4 +4,12 @@ package com.lxy.study.mq;
  * 准备检查贮藏功能
  */
 public class Test1Stash_1 {
+
+    /**
+     * 加了一个main方法
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
 }
